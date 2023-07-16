1092d348aab3dfeb04b901aec8c92d4c5d30634b
a2132176@gmail.com <a2132176@gmail.com>
2023-07-17 00:25:28 +0800

    update: key for search to do open and emit open event

--- src/App.vue
+++ src/App.vue
@@ -1,46 +1,58 @@
 <script setup>
-import { reactive, ref, computed } from "vue";
+import { reactive, ref, computed, onMounted, watch } from "vue";
 import TreeMenu from "./components/TreeMenu.vue";
 const booklist = reactive([
   {
     name: "我的書櫃",
+    isActive: false,
     children: [
       {
         name: "言情",
+        isActive: false,
         children: [
           {
             name: "西方",
+            isActive: false,
             children: [
               {
                 name: "決戰王妃",
+                isActive: false,
               },
               {
                 name: "暮光之城",
+                isActive: false,
               },
             ],
           },
           {
             name: "中國古風",
+            isActive: false,
             children: [
               {
                 name: "宮鬥",
+                isActive: false,
                 children: [
                   {
                     name: "甄嬛傳",
+                    isActive: false,
                   },
                   {
                     name: "延禧攻略",
+                    isActive: false,
                   },
                   {
                     name: "如懿傳",
+                    isActive: false,
                   },
                 ],
               },
               {
                 name: "轉生",
+                isActive: false,
                 children: [
                   {
                     name: "神醫嫡女",
+                    isActive: false,
                   },
                 ],
               },
@@ -50,29 +62,37 @@ const booklist = reactive([
       },
       {
         name: "奇幻/科幻",
+        isActive: false,
         children: [
           {
             name: "經典",
+            isActive: false,
             children: [
               {
                 name: "魔戒",
+                isActive: false,
               },
               {
                 name: "基地",
+                isActive: false,
               },
               {
                 name: "科學怪人",
+                isActive: false,
               },
             ],
           },
           {
             name: "反烏托邦",
+            isActive: false,
             children: [
               {
                 name: "我們",
+                isActive: false,
               },
               {
                 name: "美麗新世界",
+                isActive: false,
               },
             ],
           },
@@ -80,53 +100,67 @@ const booklist = reactive([
       },
       {
         name: "偵探",
+        isActive: false,
         children: [
           {
             name: "福爾摩斯",
+            isActive: false,
           },
         ],
       },
     ],
   },
   {
-    name: "我的書櫃",
+    name: "我的書櫃2",
+    isActive: false,
     children: [
       {
-        name: "言情",
+        name: "言情2",
+        isActive: false,
         children: [
           {
-            name: "西方",
+            name: "西方2",
+            isActive: false,
             children: [
               {
-                name: "決戰王妃",
+                name: "決戰王妃2",
+                isActive: false,
               },
               {
-                name: "暮光之城",
+                name: "暮光之城2",
+                isActive: false,
               },
             ],
           },
           {
-            name: "中國古風",
+            name: "中國古風2",
+            isActive: false,
             children: [
               {
-                name: "宮鬥",
+                name: "宮鬥2",
+                isActive: false,
                 children: [
                   {
-                    name: "甄嬛傳",
+                    name: "甄嬛傳2",
+                    isActive: false,
                   },
                   {
-                    name: "延禧攻略",
+                    name: "延禧攻略2",
+                    isActive: false,
                   },
                   {
-                    name: "如懿傳",
+                    name: "如懿傳2",
+                    isActive: false,
                   },
                 ],
               },
               {
-                name: "轉生",
+                name: "轉生2",
+                isActive: false,
                 children: [
                   {
-                    name: "神醫嫡女",
+                    name: "神醫嫡女2",
+                    isActive: false,
                   },
                 ],
               },
@@ -135,68 +169,87 @@ const booklist = reactive([
         ],
       },
       {
-        name: "奇幻/科幻",
+        name: "奇幻/科幻2",
+        isActive: false,
         children: [
           {
-            name: "經典",
+            name: "經典2",
+            isActive: false,
             children: [
               {
-                name: "魔戒",
+                name: "魔戒2",
+                isActive: false,
               },
               {
-                name: "基地",
+                name: "基地2",
+                isActive: false,
               },
               {
-                name: "科學怪人",
+                name: "科學怪人2",
+                isActive: false,
               },
             ],
           },
           {
-            name: "反烏托邦",
+            name: "反烏托邦2",
+            isActive: false,
             children: [
               {
-                name: "我們",
+                name: "我們2",
+                isActive: false,
               },
               {
-                name: "美麗新世界",
+                name: "美麗新世界2",
+                isActive: false,
               },
             ],
           },
         ],
       },
       {
-        name: "偵探",
+        name: "偵探2",
+        isActive: false,
         children: [
           {
-            name: "福爾摩斯",
+            name: "福爾摩斯2",
+            isActive: false,
           },
         ],
       },
     ],
   },
 ]);
-const depth = ref(0);  
+const depth = ref(0);
 const flatten = (arr) => {
-  const arr_ =  arr.flatMap(i => i.children)
+  const arr_ = arr.flatMap((i) => i.children);
+  console.log(arr_ ,'arr_')
   return arr_;
 };
 const fltternMap = computed(() => {
   return flatten(booklist);
 });
 
-const active = ref('');
+const active = ref("");
+const activeId = ref('')
 onMounted(() => {
-  console.log(active,'active')
-  active = fltternMap[0].name
+  active.value = fltternMap.value[0].name;
+  activeId.value = '0_0'
+});
+watch(active, (newVal, oldVal) => {
+  // console.log(newVal, oldVal)
+  const id = fltternMap.value.find((item)=> item.name === newVal && (item.isActive = true))
+  console.log(id.isActive, fltternMap.value, 'id')
 })
 </script>
 
 <template>
   <tree-menu
-    v-for="book in booklist"
+    v-for="(book, idx) in booklist"
     :name="book.name"
     :children="book.children"
     :depth="depth"
+    :active="activeId"
+    :data-id="`${idx}_${depth}`"
   />
   <select v-model="active">
     <option v-for="item in fltternMap">{{ item.name }}</option>
@@ -209,6 +262,10 @@ onMounted(() => {
   justify-content: center;
   .tree-menu {
     font-size: 20px;
+    display: block;
+    &.hide {
+      display: none;
+    }
     cursor: pointer;
     .category {
       color: #a49393;
--- src/components/TreeMenu.vue
+++ src/components/TreeMenu.vue
@@ -1,11 +1,12 @@
 <script setup>
-import { ref, computed, toRef, reactive  } from "vue";
+import { ref, computed, onMounted, nextTick } from "vue";
 import TreeMenu from "./TreeMenu.vue";
 
 const props = defineProps({
   name: String,
   children: Array,
   depth: Number,
+  active: String,
 });
 let isShow = ref(false);
 const canClick = computed(() => {
@@ -20,25 +21,38 @@ const indent = computed(() => {
   };
 });
 
-const toggleExpand = function () {
+const toggleExpand = async () => {
   isShow.value = !isShow.value;
+  await nextTick();
 };
+onMounted(async () => {
+  console.log(props.children, "props.children");
+  // isShow.value = props.children.children ? props.children.children.find((item) => {
+  //   console.log(item ,'itemitemitem')
+  //   return item.name === props.active
+  // }) : props.children.name === props.active
+  await nextTick();
+  console.log(isShow.value, props.active, props.depth, "isShow.value");
+});
 </script>
 
 <template>
-  <div class="tree-menu">
+  <div :class="['tree-menu', {}]">
     <div :style="indent" :class="canClick" @click="toggleExpand">
       {{ name }}
       <span v-if="children">&nbsp;{{ isShow ? "[-]" : "[+]" }}</span>
     </div>
-    <tree-menu  v-if="isShow"
-        v-for="(child, idx) in children"
-        :children="child.children"
-        :name="child.name"
-        :level="level + 1"
-        :key="`${idx}_${depth}`"
-      >
-      </tree-menu>
+    <tree-menu
+      v-show="isShow"
+      v-for="(child, idx) in children"
+      :children="child.children"
+      :name="child.name"
+      :depth="depth + 1"
+      :key="`${idx}_${depth + 1}`"
+      :data-id="`${idx}_${depth + 1}`"
+      :active="active"
+    >
+    </tree-menu>
   </div>
 </template>
 

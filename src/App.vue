769b0f5575affccc1d64c15513070b8b4312146c
Dane <dane@Danede-iMac.local>
2023-07-17 10:21:41 +0800

    feat: build github pages

--- src/App.vue
+++ src/App.vue
@@ -1,92 +1,102 @@
+<!-- @format -->
+
 <script setup>
-import { reactive, ref, computed, onMounted, watch, nextTick, toRef } from 'vue'
-import TreeMenu from './components/TreeMenu.vue'
-import { useBookStore } from './store/useStore'
-const { store: bookStore, setBookList } = useBookStore()
-const booklist = bookStore.state.booklist
-const depth = ref(0)
-const flatten = arr => {
-  if (Array.isArray(arr)) {
-    const res = [].concat.apply(
-      arr,
-      arr.map((item, id) => {
-        return item.children ? flatten(item.children) : { ...item}
-      })
-    )
-    return res
-  } else {
-    return arr
-  }
-  // return Array.isArray(arr) ?  : [arr]
-}
-
-const fltternMap = computed(() => {
-  return flatten(booklist)
-})
-
-const active = toRef(bookStore.state.active)
-const activeId = ref('')
-const handleChange = (item) => {
-  console.log({item}, 'app change')
-  item.isActive = true
-    fltternMap.value.find(_item => _item.name === item.name && (_item.isActive = true))
-    console.log(booklist, 'change')
-}
-onMounted(() => {
-  active.value =
-    fltternMap.value.find(item => item.name === active.value && (item.isActive = true))?.name ||
-    fltternMap.value[0].name
-  console.log(active, 'active')
-})
-watch(active,  (newVal, oldVal) => {
-  console.log(newVal, oldVal)
-  const id = fltternMap.value.map( item => {
-    if (item.name === newVal) {
-      item.isActive = true
-    } else {
-      item.isActive = false
+    import { reactive, ref, computed, onMounted, watch, nextTick, toRef } from 'vue'
+    import TreeMenu from './components/TreeMenu.vue'
+    import { useBookStore } from './store/useStore'
+    const { store: bookStore, setBookList } = useBookStore()
+    const booklist = bookStore.state.booklist
+    const depth = ref(0)
+    const flatten = (arr) => {
+        if (Array.isArray(arr)) {
+            const res = [].concat.apply(
+                arr,
+                arr.map((item, id) => {
+                    return item.children ? flatten(item.children) : { ...item }
+                }),
+            )
+            return res
+        } else {
+            return arr
+        }
+        // return Array.isArray(arr) ?  : [arr]
     }
 
-    setBookList(booklist)
-    console.log(item, 'booklist')
-    return item
-  })
-  localStorage.setItem('active', newVal)
-  console.log(id, fltternMap.value, 'id')
-})
+    const fltternMap = computed(() => {
+        return flatten(booklist)
+    })
+
+    const active = toRef(bookStore.state.active)
+    const activeId = ref('')
+    const handleChange = async (item) => {
+        console.log({ item }, 'app change')
+
+        await nextTick(() => {
+            item.isActive = true
+            fltternMap.value.map((_item) => {
+              if (_item.name === item.name) {
+                 active.value = _item.name
+              }
+              return (_item.isActive = _item.name === item.name)
+            })
+            console.log(booklist, 'change')
+            // active.value =
+        })
+    }
+    onMounted(() => {
+        active.value =
+            fltternMap.value.find((item) => item.name === active.value && (item.isActive = true))?.name ||
+            fltternMap.value[0].name
+        console.log(active, 'active')
+    })
+    watch(active, (newVal, oldVal) => {
+        console.log(newVal, oldVal)
+        const id = fltternMap.value.map((item) => {
+            if (item.name === newVal) {
+                item.isActive = true
+            } else {
+                item.isActive = false
+            }
+
+            setBookList(booklist)
+            console.log(item, 'booklist')
+            return item
+        })
+        localStorage.setItem('active', newVal)
+        console.log(id, fltternMap.value, 'id')
+    })
 </script>
 
 <template>
-  <tree-menu
-    v-for="(book, idx) in booklist"
-    :item="book"
-    :children="book.children"
-    :depth="depth"
-    :data-id="`${idx}_${depth}`"
-      @change="handleChange"
-  />
-  <select v-model="active">
-    <option v-for="item in fltternMap">{{ item.name }}</option>
-  </select>
+    <tree-menu
+        v-for="(book, idx) in booklist"
+        :item="book"
+        :children="book.children"
+        :depth="depth"
+        :data-id="`${idx}_${depth}`"
+        @change="handleChange" />
+    <select v-model="active">
+        <option v-for="item in fltternMap">{{ item.name }}</option>
+    </select>
 </template>
 
 <style scoped lang="scss">
-#app {
-  display: flex;
-  justify-content: center;
-  .tree-menu {
-    font-size: 20px;
-    display: block;
-    &.hide {
-      display: none;
+    #app {
+        display: flex;
+        justify-content: center;
+        .tree-menu {
+            font-size: 20px;
+            display: block;
+            &.hide {
+                display: none;
+            }
+            cursor: pointer;
+            .category {
+                color: #a49393;
+            }
+            .book {
+                color: #67595e;
+            }
+        }
     }
-    cursor: pointer;
-    .category {
-      color: #a49393;
-    }
-    .book {
-      color: #67595e;
-    }
-  }
-}
 </style>
--- src/components/TreeMenu.vue
+++ src/components/TreeMenu.vue
@@ -1,74 +1,71 @@
+<!-- @format -->
+
 <script setup>
-import { ref, computed, onMounted, nextTick, watchEffect, defineEmits } from 'vue'
-import TreeMenu from './TreeMenu.vue'
+    import { ref, computed, onMounted, nextTick } from 'vue'
+    import TreeMenu from './TreeMenu.vue'
 
-const props = defineProps({
-  item: Object,
-  children: Array,
-  depth: Number,
-})
-const emit = defineEmits(['change'])
-let isShow = ref(false)
-const canClick = computed(() => {
-  return {
-    category: props.children,
-    book: !props.children
-  }
-})
-const indent = computed(() => {
-  return {
-    transform: `translate(${props.depth * 30}px)`
-  }
-})
+    const props = defineProps({
+        item: Object,
+        children: Array,
+        depth: Number,
+    })
+    const emit = defineEmits(['change'])
+    let isShow = ref(false)
+    const canClick = computed(() => {
+        return {
+            category: props.children,
+            book: !props.children,
+        }
+    })
+    const indent = computed(() => {
+        return {
+            transform: `translate(${props.depth * 30}px)`,
+        }
+    })
 
-const findDeep = (arr, val) => {
-  console.log(arr, Array.isArray(arr))
-  return arr.find(item => {
-    if (item.name === val) {
-      item.isActive = true
-    } else {
-      findDeep(item.children, val)
+    const findDeep = (arr, val) => {
+        console.log(arr, Array.isArray(arr))
+        return arr.find((item) => {
+            if (item.name === val) {
+                item.isActive = true
+            } else {
+                findDeep(item.children, val)
+            }
+            return item
+        })
     }
-    return item
-  })
-}
 
-const toggleExpand = async() => {
-  isShow.value = !isShow.value
-  emit('change', props.item)
-}
-const handleChange = (item) => {
-
-}
-onMounted(async () => {
-  await nextTick()
-      console.log(props.children, 'props.children')
-      isShow.value =
-        (props.children || []).filter(item => {
-          return item.isActive == true
-        }).length > 0 || props.item.isActive
-      
-})
+    const toggleExpand = async () => {
+        isShow.value = !isShow.value
+        emit('change', props.item)
+    }
+    const handleChange = (item) => {}
+    onMounted(async () => {
+        await nextTick()
+        isShow.value = props.item.isActive
+    })
 </script>
 
 <template>
-  <div :class="['tree-menu']">
-    <div :style="indent" :class="canClick" @click="toggleExpand">
-      {{ item.name }}
-      <span v-if="children">&nbsp;{{ isShow ? '[-]' : '[+]' }}</span>
+    <div :class="['tree-menu']">
+        <div
+            :style="indent"
+            :class="canClick"
+            @click="toggleExpand">
+            {{ item.name }}
+            <span v-if="children">&nbsp;{{ isShow ? '[-]' : '[+]' }}</span>
+        </div>
+        <tree-menu
+            v-show="isShow"
+            v-for="(child, idx) in children"
+            :children="child.children"
+            :item="child"
+            :depth="depth + 1"
+            :key="`${idx}_${depth + 1}`"
+            :data-id="`${idx}_${depth + 1}`"
+            @change="handleChange">
+        </tree-menu>
     </div>
-    <tree-menu
-      v-show="isShow"
-      v-for="(child, idx) in children"
-      :children="child.children"
-      :item="child"
-      :depth="depth + 1"
-      :key="`${idx}_${depth + 1}`"
-      :data-id="`${idx}_${depth + 1}`"
-      @change="handleChange"
-    >
-    </tree-menu>
-  </div>
 </template>
 
 <style scoped></style>

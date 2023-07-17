fba26dd2756ba592b35d6b15dab63dc9be7370b1
Dane <a2132176@gmail.com>
2023-07-17 21:11:33 +0800

    fix: step2&3

--- src/App.vue
+++ src/App.vue
@@ -1,108 +1,102 @@
 <!-- @format -->
 
 <script setup>
-    import { reactive, ref, computed, onMounted, watch, nextTick, toRef } from 'vue'
-    import TreeMenu from './components/TreeMenu.vue'
-    import { useBookStore } from './store/useStore'
-    const { store: bookStore, setBookList } = useBookStore()
-    const booklist = bookStore.state.booklist
-    const depth = ref(0)
-    const flatten = (arr, gId = -1, deep = 0) => {
-        if (Array.isArray(arr)) {
-            gId != -1 && deep++
-            const res = [].concat.apply(
-                arr,
-                arr.map((item, id) => {
-                    item.gId = deep === 0 ? id : gId
-                    item.deep = deep
-                    return (item.children && flatten(item.children, deep === 0 ? id : gId, deep)) || item
-                }),
-            )
-            return res
-        }
-        // return Array.isArray(arr) ?  : [arr]
+import { reactive, ref, computed, onMounted, watch, nextTick, toRef } from 'vue'
+import TreeMenu from './components/TreeMenu.vue'
+import { useBookStore } from './store/useStore'
+const { store: bookStore, setBookList } = useBookStore()
+const booklist = bookStore.state.booklist
+const depth = ref(0)
+const flatten = (arr, gId = -1, deep = 0) => {
+  if (Array.isArray(arr)) {
+    gId != -1 && deep++
+    const res = [].concat.apply(
+      arr,
+      arr.map((item, id) => {
+        item.gId = deep === 0 ? id : gId
+        item.deep = deep
+        return (
+          (item.children &&
+            flatten(item.children, deep === 0 ? id : gId, deep)) ||
+          item
+        )
+      })
+    )
+    return res
+  }
+  // return Array.isArray(arr) ?  : [arr]
+}
+
+const fltternMap = computed(() => {
+  return flatten(booklist)
+})
+
+const active = ref('')
+const activeId = ref('')
+
+const randomId = ref(0)
+const handleChange = async item => {
+  console.log({ item }, 'app change')
+  // item.isActive = !item.isActive
+  active.value = item.name
+  console.log(fltternMap, 'fltternMap')
+  // fltternMap.value.map((_item) => {
+  //     console.log(_item, '_item')
+  //     if (_item.name === item.name) {
+  //         active.value = _item.name
+  //     }
+  //     console.log(_item.gId === item.gId, ' _item.gId === item.gId')
+  //     return (_item.isActive = _item.gId === item.gId)
+  // })
+  await nextTick()
+}
+onMounted(() => {
+  const defauleActive = localStorage.getItem('active') || ''
+  active.value =
+    fltternMap.value.find(
+      item => item.name === defauleActive && (item.isActive = true)
+    )?.name || fltternMap.value[0].name
+  console.log(active, 'active')
+})
+watch(active, (newVal, oldVal) => {
+  console.log(newVal, oldVal)
+  const activeObj = fltternMap.value.find(item => item.name === newVal)
+  console.log(activeObj, 'activeObj')
+  const id = fltternMap.value.map(item => {
+    if (item.gId === activeObj.gId && item.deep <= activeObj.deep) {
+      item.isActive = true
+    } else {
+      item.isActive = false
     }
 
-    const fltternMap = computed(() => {
-        return flatten(booklist)
-    })
-
-    const active = toRef(bookStore.state.active)
-    const activeId = ref('')
-
-    const randomId = ref(0)
-    const handleChange = async (item) => {
-        console.log({ item }, 'app change')
-        item.isActive = true
-        console.log(fltternMap, 'fltternMap')
-        // fltternMap.value.map((_item) => {
-        //     console.log(_item, '_item')
-        //     if (_item.name === item.name) {
-        //         active.value = _item.name
-        //     }
-        //     console.log(_item.gId === item.gId, ' _item.gId === item.gId')
-        //     return (_item.isActive = _item.gId === item.gId)
-        // })
-        await nextTick()
-        randomId.value = Math.random() * 100
-    }
-    onMounted(() => {
-        active.value =
-            fltternMap.value.find((item) => item.name === active.value && (item.isActive = true))?.name ||
-            fltternMap.value[0].name
-        console.log(active, 'active')
-    })
-    watch(active, (newVal, oldVal) => {
-        console.log(newVal, oldVal)
-        const activeObj =  fltternMap.value.find((item) => item.name === newVal)
-        console.log(activeObj, 'activeObj')
-        const id = fltternMap.value.map((item) => {
-            if (item.gId === activeObj.gId  && item.deep <= activeObj.deep) {
-                item.isActive = true
-            } else {
-                item.isActive = false
-            }
-
-            // setBookList(booklist)
-            // console.log(item, 'booklist')
-            return item
-        })
-        localStorage.setItem('active', newVal)
-        console.log(id, fltternMap.value, 'id')
-    })
+    // setBookList(booklist)
+    // console.log(item, 'booklist')
+    return item
+  })
+  localStorage.setItem('active', newVal)
+  console.log(id, fltternMap.value, 'id')
+})
 </script>
 
 <template>
-    <tree-menu
-        v-for="(book, idx) in booklist"
-        :item="book"
-        :children="book.children"
-        :depth="depth"
-        :data-id="`${idx}_${depth}_${randomId}`"
-        @change="handleChange"
-        ref="MainTreeMenu" />
-    <select v-model="active">
-        <option v-for="item in fltternMap">{{ item.name }}</option>
-    </select>
+  <tree-menu
+    v-for="(book, idx) in booklist"
+    :item="book"
+    :children="book.children"
+    :depth="depth"
+    :data-id="`${idx}_${depth}_${randomId}`"
+    @change="handleChange"
+    :active="active"
+    ref="MainTreeMenu"
+  />
+  <select v-model="active">
+    <option v-for="item in fltternMap">{{ item.name }}</option>
+  </select>
 </template>
 
 <style scoped lang="scss">
-    #app {
-        display: flex;
-        justify-content: center;
-        .tree-menu {
-            font-size: 20px;
-            display: block;
-            &.hide {
-                display: none;
-            }
-            cursor: pointer;
-            .category {
-                color: #a49393;
-            }
-            .book {
-                color: #67595e;
-            }
-        }
-    }
+#app {
+  display: flex;
+  justify-content: center;
+}
 </style>
--- src/components/TreeMenu.vue
+++ src/components/TreeMenu.vue
@@ -1,75 +1,94 @@
 <!-- @format -->
 
 <script setup>
-    import { ref, computed, onMounted, nextTick } from 'vue'
-    import TreeMenu from './TreeMenu.vue'
+import { ref, computed, onMounted, nextTick, toRef } from 'vue'
+import TreeMenu from './TreeMenu.vue'
 
-    const props = defineProps({
-        item: Object,
-        children: Array,
-        depth: Number,
-    })
-    const emit = defineEmits(['change'])
-    let isShow = ref(false)
-    const canClick = computed(() => {
-        return {
-            category: props.children,
-            book: !props.children,
-        }
-    })
-    const indent = computed(() => {
-        return {
-            transform: `translate(${props.depth * 30}px)`,
-        }
-    })
+const props = defineProps({
+  item: Object,
+  children: Array,
+  depth: Number,
+  active: String
+})
+const emit = defineEmits(['change'])
+let isShow = ref(false)
+const canClick = computed(() => {
+  return {
+    category: props.children,
+    book: !props.children
+  }
+})
+const indent = computed(() => {
+  return {
+    transform: `translate(${props.depth * 30}px)`
+  }
+})
 
-    const findDeep = (arr, val) => {
-        console.log(arr, Array.isArray(arr))
-        return arr.find((item) => {
-            if (item.name === val) {
-                item.isActive = true
-            } else {
-                findDeep(item.children, val)
-            }
-            return item
-        })
+const findDeep = (arr, val) => {
+  console.log(arr, Array.isArray(arr))
+  return arr.find(item => {
+    if (item.name === val) {
+      item.isActive = true
+    } else {
+      findDeep(item.children, val)
     }
+    return item
+  })
+}
 
-    const toggleExpand = async () => {
-        // isShow.value = !isShow.value
-        props.item.isActive = !props.item.isActive
-        emit('change', props.item)
-    }
-    const handleChange = (item) => {
-        console.log('item: ', item);
-       props.item.isActive = true
-    }
-    onMounted(async () => {
-        // await nextTick()
-        // isShow.value = props.item.isActive
-    })
+const toggleExpand = () => {
+  isShow.value = !isShow.value
+  props.item.isActive = !props.item.isActive
+  emit('change', props.item)
+}
+const handleChange = item => {
+  console.log('item: ', item)
+  props.item.isActive = true
+}
+onMounted(async () => {
+  // await nextTick()
+  // isShow.value = props.item.isActive
+})
 </script>
 
 <template>
-    <div :class="['tree-menu']">
-        <div
-            :style="indent"
-            :class="canClick"
-            @click="toggleExpand">
-            {{ item.name }}
-            <span v-if="children">&nbsp;{{ item.isActive ? '[-]' : '[+]' }}</span>
-        </div>
-        <tree-menu
-            v-show="item.isActive"
-            v-for="(child, idx) in children"
-            :children="child.children"
-            :item="child"
-            :depth="depth + 1"
-            :key="`${idx}_${depth + 1}`"
-            :data-id="`${idx}_${depth + 1}`"
-            @change="handleChange">
-        </tree-menu>
+  <div :class="['tree-menu', { actived: active == item.name }]">
+    <div :style="indent" :class="canClick" @click="toggleExpand">
+      {{ item.name }}
+      <span v-if="children">&nbsp;{{ item.isActive ? '[-]' : '[+]' }}</span>
     </div>
+    <tree-menu
+      v-show="item.isActive"
+      v-for="(child, idx) in children"
+      :children="child.children"
+      :item="child"
+      :depth="depth + 1"
+      :key="`${idx}_${depth + 1}`"
+      :data-id="`${idx}_${depth + 1}`"
+      :active="active"
+      @change="handleChange"
+    >
+    </tree-menu>
+  </div>
 </template>
 
-<style scoped></style>
+<style scoped lang="scss">
+.tree-menu {
+  font-size: 20px;
+  display: block;
+  &.actived > .category {
+    color: yellow;
+    background: black;
+  }
+  &.hide {
+    display: none;
+  }
+  cursor: pointer;
+  .category {
+    color: #a49393;
+  }
+  .book {
+    color: #67595e;
+  }
+}
+</style>
